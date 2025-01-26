19f27d12cc42408f0183a8701eaeb41b9b864ce4
Bryan P <bryanpsd@gmail.com>
2025-01-26 07:55:17 -0800

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    plugins: [vanillaExtractPlugin()],
+  },
+});
