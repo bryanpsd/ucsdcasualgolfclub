037c83d6a7cd0a49cdae0ea2e78c4e1ba3223361
Bryan P <bryanpsd@gmail.com>
2025-04-23 16:34:31 -0700

    updates

--- src/pages/roster/_components/Select/Select.tsx
+++ src/pages/roster/_components/Select/Select.tsx
@@ -1,35 +1,31 @@
-import React from "react";
+import React from 'react';
 
 interface SelectProps {
   options: string[];
   currentOption?: string;
   label?: string;
-  onChangeUrl: string;
+  onChangeUrl: (selectedYear: string) => string;
 }
 
 export const Select: React.FC<SelectProps> = ({
   options,
   currentOption,
-  label = "Select Year",
+  label = 'Select Year',
   onChangeUrl,
 }) => {
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedYear = event.target.value;
     if (selectedYear) {
-      window.location.href = `${onChangeUrl}/${selectedYear}`;
+      window.location.href = onChangeUrl(selectedYear);
     }
   };
 
   return (
     <div>
       {label && <label htmlFor="year-select">{label}</label>}
-      <select id="year-select" onChange={handleChange}>
+      <select id="year-select" onChange={handleChange} value={currentOption}>
         {options.map((option) => (
-          <option
-            key={option}
-            value={option}
-            selected={option === currentOption}
-          >
+          <option key={option} value={option}>
             {option}
           </option>
         ))}
