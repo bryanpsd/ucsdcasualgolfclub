990f16cabf79e4584b860f65599bc74c978ed115
Bryan P <bryanpsd@gmail.com>
2025-04-26 07:05:27 -0700

    Updates

--- src/components/Course/CourseInclusions/CourseInclusions.css.ts
+++ src/components/Course/CourseInclusions/CourseInclusions.css.ts
@@ -1,39 +1,70 @@
-import { style } from "@vanilla-extract/css";
-import { tokens } from "../../../styles/designTokens.css";
-import { color } from "../../../styles/designTokens/colors";
-import { recipe } from "@vanilla-extract/recipes";
+import { style } from '@vanilla-extract/css';
+import { tokens } from '../../../styles/designTokens.css';
+import { color } from '../../../styles/designTokens/colors';
+import { recipe } from '@vanilla-extract/recipes';
 
-export const courseInclusionsWrapper = style([
-  tokens({
-    display: "flex",
-    flexDirection: "column",
-    gap: 4,
-  }),
-]);
+export const courseInclusionsWrapper = recipe({
+  base: [
+    tokens({
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 4,
+    }),
+  ],
+  variants: {
+    variant: {
+      default: {},
+      secondary: {
+        color: color.brand.white,
+      },
+    },
+  },
+});
 
-export const courseInclusionsList = style([
-  tokens({
-    display: "flex",
-    flexDirection: "row",
-    gap: { "xs-min": 8, "lg-min": 0 },
-    flexWrap: { "xs-min": "wrap", "lg-min": "nowrap" },
-  }),
-]);
+export const courseInclusionsHeadline = recipe({
+  variants: {
+    variant: {
+      secondary: {
+        color: color.brand.white,
+      },
+    },
+  },
+});
+
+export const courseInclusionsList = recipe({
+  base: [
+    tokens({
+      display: 'flex',
+      flexDirection: 'row',
+      gap: { 'xs-min': 8, 'lg-min': 0 },
+      flexWrap: { 'xs-min': 'wrap', 'lg-min': 'nowrap' },
+    }),
+  ],
+  variants: {
+    variant: {
+      default: {},
+      secondary: {
+        color: color.brand.white,
+        justifyContent: 'center',
+      },
+    },
+  },
+});
 
 export const courseInclusionsListItem = recipe({
   base: [
     tokens({
-      display: "flex",
+      display: 'flex',
       paddingX: 4,
-      alignItems: "center",
+      alignItems: 'center',
     }),
     {
       selectors: {
-        "&:first-child": {
+        '&:first-child': {
           paddingLeft: 0,
         },
-        "&:last-child": {
-          borderRight: "none",
+        '&:last-child': {
+          borderRight: 'none',
         },
       },
     },
@@ -46,6 +77,19 @@ export const courseInclusionsListItem = recipe({
       secondary: {
         borderRight: `1px solid ${color.brand.white}`,
         color: color.brand.white,
+      },
+    },
+  },
+});
+
+export const courseInclusionsIcon = recipe({
+  variants: {
+    variant: {
+      default: {
+        fill: color.brand.navy,
+      },
+      secondary: {
+        fill: color.brand.white,
       },
     },
   },
