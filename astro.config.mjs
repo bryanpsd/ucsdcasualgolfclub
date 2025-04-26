88344fb6d2aab5497bf62542062b74edc3c3afe9
Bryan P <bryanpsd@gmail.com>
2025-04-26 07:15:54 -0700

    Added netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'astro/config';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import netlify from '@astrojs/netlify';
+import svgr from 'vite-plugin-svgr';
 
-import react from "@astrojs/react";
-import icon from "astro-icon";
+import react from '@astrojs/react';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [react(), icon()],
   vite: {
     plugins: [vanillaExtractPlugin(), svgr()],
