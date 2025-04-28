f5811b0fcc5db1a6736fabe43c3713d59ad33574
Bryan P <bryanpsd@gmail.com>
2025-04-28 08:57:07 -0700

    new files

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   adapter: netlify(),
   integrations: [react(), icon()],
   vite: {
