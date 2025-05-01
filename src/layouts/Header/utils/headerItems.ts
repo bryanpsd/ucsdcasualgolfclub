27ce0428b7f3deb53d6b4f454c84b791bdbbed48
Bryan <bryanpsd@gmail.com>
2025-05-01 11:05:10 -0700

    Updated nav it for seasons

--- src/layouts/Header/utils/headerItems.ts
+++ src/layouts/Header/utils/headerItems.ts
@@ -1,14 +1,60 @@
 import type { MainNavProps } from '../components/MainNav'
+import { contentfulClient } from '../../../lib/contentful'
 
-// Generate dynamic links for Seasons
-const getYear = new Date().getFullYear() - 1
-const pastSeasonsLinks = Array.from({ length: getYear - 2018 }, (_, i) => {
-  const year = getYear - i
-  return {
+const getAvailableSeasons = async () => {
+  const entries = await contentfulClient.getEntries({
+    content_type: 'course',
+    include: 1,
+  })
+
+  const seasonsWithTournaments = entries.items
+    .flatMap((item) =>
+      Array.isArray(item.fields.tournaments)
+        ? item.fields.tournaments.filter(
+            (tournament) => typeof tournament === 'object' && tournament !== null
+          )
+        : []
+    )
+    .filter((tournament) => tournament !== undefined && tournament !== null)
+
+  const uniqueYears = Array.from(
+    new Set(
+      seasonsWithTournaments.map((tournament) => {
+        if (
+          'fields' in tournament &&
+          tournament.fields &&
+          typeof tournament.fields === 'object' &&
+          'date' in tournament.fields
+        ) {
+          return typeof tournament.fields.date === 'string' ||
+            typeof tournament.fields.date === 'number'
+            ? new Date(tournament.fields.date).getFullYear()
+            : null
+        }
+        return null
+      })
+    )
+  ).filter((year) => year !== null)
+
+  return uniqueYears.sort((a, b) => b - a)
+}
+
+const generatePastSeasonsLinks = async () => {
+  const availableSeasons = await getAvailableSeasons()
+
+  // Get the current year
+  const currentYear = new Date().getFullYear()
+
+  // Filter out the current year
+  const filteredSeasons = availableSeasons.filter((year) => year !== currentYear)
+
+  return filteredSeasons.map((year) => ({
     label: year.toString(),
     href: `/seasons/${year}`,
-  }
-})
+  }))
+}
+
+const pastSeasonsLinks = await generatePastSeasonsLinks()
 
 export const menuItems: MainNavProps['items'] = {
   label: 'Main',
