9be42415e9be67a955fa79505600115b56d2cc6b
Bryan P <bryanpsd@gmail.com>
2025-05-01 20:47:52 -0700

    Removed file

--- src/types/react-arguments.ts
+++
@@ -1,7 +0,0 @@
-import type { ForwardedRef } from 'react'
-
-declare module 'react' {
-  function forwardRef<T, P = unknown>(
-    render: (props: P, ref: ForwardedRef<T>) => ReactElement | null
-  ): (props: P & RefAttributes<T>) => ReactElement | null
-}
