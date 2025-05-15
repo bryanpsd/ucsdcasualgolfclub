724e18008f14dce96c79245d1b0b1848f183eb03
Bryan <bryanpsd@gmail.com>
2025-05-15 08:57:50 -0700

    Added robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,25 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import netlify from '@astrojs/netlify'
 import svgr from 'vite-plugin-svgr'
-
+import robotsTxt from 'astro-robots-txt'
 import react from '@astrojs/react'
 import icon from 'astro-icon'
 
 import partytown from '@astrojs/partytown'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://ucsdcasualgolfclub.com',
   adapter: netlify(),
   output: 'server',
-  integrations: [react(), icon(), partytown({ config: { forward: ['dataLayer.push'] } }), sitemap()],
+  integrations: [
+    react(),
+    icon(),
+    partytown({ config: { forward: ['dataLayer.push'] } }),
+    sitemap(),
+    robotsTxt(),
+  ],
   vite: {
     plugins: [viteTsconfigPaths(), vanillaExtractPlugin(), svgr()],
     assetsInclude: ['**/*.ttf'], // Ensure .ttf files are included as assets
