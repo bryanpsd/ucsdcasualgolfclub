866f35ccded8d9734055b7761cbce6042d3d20cf
Bryan P <bryanpsd@gmail.com>
2025-09-12 06:20:26 -0700

    Added pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,27 @@
-import netlify from '@astrojs/netlify'
-import partytown from '@astrojs/partytown'
-import react from '@astrojs/react'
-import sitemap from '@astrojs/sitemap'
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
-import { defineConfig } from 'astro/config'
-import icon from 'astro-icon'
-import robotsTxt from 'astro-robots-txt'
-import svgr from 'vite-plugin-svgr'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
+import netlify from "@astrojs/netlify"
+import partytown from "@astrojs/partytown"
+import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin"
+import { defineConfig } from "astro/config"
+import icon from "astro-icon"
+import robotsTxt from "astro-robots-txt"
+import svgr from "vite-plugin-svgr"
+import viteTsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-	site: 'https://ucsdcasualgolfclub.com',
+	site: "https://ucsdcasualgolfclub.com",
 	adapter: netlify(),
-	output: 'server',
+	output: "server",
 	integrations: [
 		react(),
 		icon(),
-		partytown({ config: { forward: ['dataLayer.push'] } }),
+		partytown({ config: { forward: ["dataLayer.push"] } }),
 		sitemap(),
 		robotsTxt(),
 	],
 	vite: {
 		plugins: [viteTsconfigPaths(), vanillaExtractPlugin(), svgr()],
-		assetsInclude: ['**/*.ttf'], // Ensure .ttf files are included as assets
+		assetsInclude: ["**/*.ttf"], // Ensure .ttf files are included as assets
 	},
 })
