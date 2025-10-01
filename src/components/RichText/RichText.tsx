d46c1678247af39b86bd8cac3b3dc442845ed2a9
Bryan P <bryanpsd@gmail.com>
2025-10-01 15:55:33 -0700

    Fix rich text

--- src/components/RichText/RichText.tsx
+++ src/components/RichText/RichText.tsx
@@ -1,6 +1,5 @@
 import type { Options } from "@contentful/rich-text-react-renderer"
 import type { Document } from "@contentful/rich-text-types"
-import { BLOCKS } from "@contentful/rich-text-types"
 import { ContentfulImage } from "~components/Image/ContentfulImage"
 import { ResponsiveHeadline } from "~components/ResponsiveHeadline"
 import { TextBlockSection } from "~components/TextBlockSection"
@@ -9,6 +8,29 @@ import type { TypographyProps } from "~components/Typography"
 import { Typography } from "~components/Typography"
 
 import * as styles from "./RichText.css"
+
+// Contentful rich text constants (ESM-compatible)
+const BLOCKS = {
+	DOCUMENT: "document",
+	PARAGRAPH: "paragraph",
+	HEADING_1: "heading-1",
+	HEADING_2: "heading-2",
+	HEADING_3: "heading-3",
+	HEADING_4: "heading-4",
+	HEADING_5: "heading-5",
+	HEADING_6: "heading-6",
+	OL_LIST: "ordered-list",
+	UL_LIST: "unordered-list",
+	LIST_ITEM: "list-item",
+	QUOTE: "blockquote",
+	HR: "hr",
+	EMBEDDED_ENTRY: "embedded-entry-block",
+	EMBEDDED_ASSET: "embedded-asset-block",
+	TABLE: "table",
+	TABLE_ROW: "table-row",
+	TABLE_CELL: "table-cell",
+	TABLE_HEADER_CELL: "table-header-cell",
+}
 
 const BODY_TYPOGRAPHY_VARIANT: TypographyProps["variant"] = "bodyMd"
 
