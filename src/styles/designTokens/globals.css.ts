438218e4bf4ffc11014c36b8810673a685d744c1
Bryan <bryanpsd@gmail.com>
2025-10-07 12:11:19 -0700

    changing the way fonts are

--- src/styles/designTokens/globals.css.ts
+++ src/styles/designTokens/globals.css.ts
@@ -5,30 +5,35 @@ globalStyle("html", {
   boxSizing: "border-box",
   WebkitFontSmoothing: "antialiased",
   MozOsxFontSmoothing: "grayscale",
+  fontFamily: "Roboto, Arial, sans-serif",
 })
 
 globalStyle("*, *::after, *::before", {
   boxSizing: "inherit",
-  fontFamily: "Roboto, Arial, sans-serif",
-})
-
-globalStyle("strong, b", {
-	fontWeight: 700,
 })
 
 globalStyle("body", {
-	color: "#000000",
-	margin: 0,
-	fontSize: "1.2rem",
+  color: "#000000",
+  margin: 0,
+  fontSize: "1rem",
+  lineHeight: 1.5,
+  fontFamily: "inherit",
+  background: "#fff",
+  WebkitFontSmoothing: "antialiased",
+  MozOsxFontSmoothing: "grayscale",
+})
+
+globalStyle("strong, b", {
+  fontWeight: 700,
 })
 
 globalStyle(".sr-only", {
-	position: "absolute",
-	width: "1px",
-	height: "1px",
-	padding: 0,
-	margin: "-1px",
-	overflow: "hidden",
-	clip: "rect(0,0,0,0)",
-	border: 0,
+  position: "absolute",
+  width: "1px",
+  height: "1px",
+  padding: 0,
+  margin: "-1px",
+  overflow: "hidden",
+  clip: "rect(0,0,0,0)",
+  border: 0,
 })
--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,65 +1,10 @@
 import { constants } from "./constants"
 
-const fonts = [
-	"/fonts/Roboto-Regular.ttf",
-	"/fonts/Roboto-Light.ttf",
-	"/fonts/Roboto-Medium.ttf",
-	"/fonts/Roboto-Bold.ttf",
-	"/fonts/Roboto-Black.ttf",
-	"/fonts/Roboto-Italic.ttf",
-	"/fonts/Roboto-LightItalic.ttf",
-	"/fonts/Roboto-MediumItalic.ttf",
-	"/fonts/Roboto-BoldItalic.ttf",
-	"/fonts/Roboto-BlackItalic.ttf",
-]
-
-/**
- * Generates a fontFace rule from a fontUrl and fontName
- * @param fontUrl Url to font file
- * @param fontName Name of font family collection
- * @returns @fontFace CSS rule
- */
-const fontFaceGenerator = (fontUrl: string, fontName: string) => {
-	let fontWeight: number
-	switch (true) {
-		case fontUrl.toLocaleLowerCase().includes("light"):
-			fontWeight = 300
-			break
-		case fontUrl.toLocaleLowerCase().includes("medium"):
-			fontWeight = 500
-			break
-		case fontUrl.toLocaleLowerCase().includes("bold"):
-			fontWeight = 700
-			break
-		case fontUrl.toLocaleLowerCase().includes("black"):
-			fontWeight = 900
-			break
-		default:
-			fontWeight = 400
-			break
-	}
-	const fontStyle = fontUrl.toLocaleLowerCase().includes("italic")
-		? "italic"
-		: "normal"
-
-	return {
-		fontFamily: fontName,
-		fontStyle,
-		fontWeight,
-		fontDisplay: "swap",
-		src: `url(${fontUrl}) format('woff2')`,
-	} as const
-}
-
-const ucsdRobotoFontFaces = fonts.map((url) => {
-	return fontFaceGenerator(url, "Roboto")
-})
-
 const typography = {
-	// @material-ui/core/styles/createTypography.d.ts
-	htmlFontSize: 10, // html.fontSize set to 62.5% in `CssBaseline` override
-	fontFamily: ["Roboto", "serif"].join(", "),
-	...constants.typography,
+  // @material-ui/core/styles/createTypography.d.ts
+  htmlFontSize: 10, // html.fontSize set to 62.5% in `CssBaseline` override
+  fontFamily: "Roboto, Arial, sans-serif",
+  ...constants.typography,
 }
 
-export { typography, ucsdRobotoFontFaces }
+export { typography }
