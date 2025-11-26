079ab0389972bdcbf04ca00221d120eec2a7faa6
Bryan P <bryanpsd@gmail.com>
2025-11-26 12:12:20 -0800

    Testing e2e

--- src/services/contentful/contentful.ts
+++ src/services/contentful/contentful.ts
@@ -13,8 +13,11 @@ export interface SimplePage {
 
 export const contentfulClient = contentful.createClient({
 	space: import.meta.env.CONTENTFUL_SPACE_ID,
-	accessToken: import.meta.env.DEV
-		? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-		: import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-	host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+	accessToken:
+		// In CI environments, always use delivery token even in dev mode
+		import.meta.env.CI || !import.meta.env.DEV
+			? import.meta.env.CONTENTFUL_DELIVERY_TOKEN
+			: import.meta.env.CONTENTFUL_PREVIEW_TOKEN,
+	host:
+		import.meta.env.CI || !import.meta.env.DEV ? "cdn.contentful.com" : "preview.contentful.com",
 });
